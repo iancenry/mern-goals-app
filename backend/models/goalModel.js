a9bc5150ff80279b2d4371077b2b70da357c6afe
Ian Cenry <iancenry@gmail.com>
2023-09-10 17:18:37 +0300

    chore: add goal-user relationship

--- backend/models/goalModel.js
+++ backend/models/goalModel.js
@@ -2,6 +2,13 @@ const mongoose = require('mongoose');
 
 const goalSchema = mongoose.Schema(
   {
+    user: {
+      //the _id created in the db
+      type: mongoose.Schema.Types.ObjectId,
+      required: true,
+      //ObjectId to reference User Model; User associated with a goal
+      ref: 'User',
+    },
     text: {
       type: String,
       required: [true, 'Please add a text value'],
