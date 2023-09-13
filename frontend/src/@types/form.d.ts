d832676fa02e11b0fe6f433d18ce0dd1991bed0a
Ian Cenry <iancenry@gmail.com>
2023-09-13 22:57:18 +0300

    define types

---
+++ frontend/src/@types/form.d.ts
@@ -0,0 +1,11 @@
+export interface IRegisterFormData {
+  name: string
+  email: string
+  password: string
+  confirmPassword: string
+}
+
+export interface ILoginForm {
+  email: string
+  password: string
+}
