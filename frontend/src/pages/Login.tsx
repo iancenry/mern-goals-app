583fb8f2045e71b7fbe1458d5a473d3763b72d8d
Ian Cenry <iancenry@gmail.com>
2023-09-13 23:02:13 +0300

    chore: update UI

--- frontend/src/pages/Login.tsx
+++ frontend/src/pages/Login.tsx
@@ -1,5 +1,68 @@
-const Login = () => {
-  return <div>Login</div>;
-};
+import {
+  Form,
+  // useLoaderData,
+  // redirect,
+  // useActionData,
+  useNavigation,
+} from "react-router-dom"
+import { FaSign } from "react-icons/fa"
 
-export default Login;
+export async function action({ request }: { request: Request }) {
+  const formData: FormData = await request.formData()
+  const email = formData.get("email")
+  const password = formData.get("password")
+  console.log(email, password)
+  // // get url that directed user to login page; default to host route
+  // const previousPathname =
+  //   new URL(request.url).searchParams.get("redirectTo") || "/register"
+
+  try {
+    // const data = await loginUser({ email, password })
+    // localStorage.setItem("loggedIn", true)
+    // return redirect(previousPathname)
+  } catch (error) {
+    // return error.message
+  }
+
+  return null
+}
+
+const Login = () => {
+  // const errorMessage = useActionData()
+  // const message = useLoaderData()
+  const navigation = useNavigation()
+
+  return (
+    <>
+      <section className="heading">
+        <h1>
+          <FaSign /> Login
+        </h1>
+        <p>Login and start setting goals</p>
+      </section>
+
+      <section className="form">
+        {/* {errorMessage && <h3 className="red">{errorMessage}</h3>} */}
+        {/* {message && <h3 className="red">{message}</h3>} */}
+        <Form method="post">
+          <div className="form-group">
+            <input type="email" name="email" placeholder="Email Address" />
+          </div>
+          <div className="form-group">
+            <input type="password" name="password" placeholder="Password" />
+          </div>
+          <div className="form-group">
+            <button
+              className="btn btn-block"
+              disabled={navigation.state === "submitting"}
+            >
+              {navigation.state === "submitting" ? "Logging In..." : "Log In"}
+            </button>
+          </div>
+        </Form>
+      </section>
+    </>
+  )
+}
+
+export default Login
--- frontend/src/pages/Register.tsx
+++ frontend/src/pages/Register.tsx
@@ -1,5 +1,110 @@
-const Register = () => {
-  return <div>Register</div>;
-};
+import { Form, redirect, useNavigation } from "react-router-dom"
+import { IRegisterFormData } from "../@types/form"
+import { FaUser } from "react-icons/fa"
 
-export default Register;
+export async function registerAction({ request }: { request: Request }) {
+  const formData: FormData = await request.formData()
+  const name = formData.get("name")
+  const email = formData.get("email")
+  const password = formData.get("password")
+  const confirmPassword = formData.get("confirmPassword")
+
+  console.log(name, email, password, confirmPassword)
+
+  const errors: IRegisterFormData = {} as IRegisterFormData
+
+  //validate the fields - switch with regex
+  if (typeof name !== "string" || name.length < 6) {
+    errors.name = "Please provide your real name"
+  }
+
+  if (typeof email !== "string" || !email.includes("@")) {
+    errors.email = "That doesn't look like an email address"
+  }
+
+  if (typeof password !== "string" || password.length < 6) {
+    errors.password = "Password must be > 6 characters"
+  }
+
+  if (password !== confirmPassword) {
+    errors.confirmPassword = "Passwords do not match"
+  }
+
+  // return data if we have errors
+  if (Object.keys(errors).length) {
+    console.log(errors)
+    return errors
+  }
+
+  // create the user and redirect
+  // await createUser(email, password)
+  // return redirect("/dashboard")
+  return null
+}
+
+const Register = () => {
+  const navigation = useNavigation()
+
+  return (
+    <>
+      <section className="heading">
+        <h1>
+          <FaUser /> Register
+        </h1>
+        <p>Please create an account</p>
+      </section>
+
+      <section className="form">
+        <Form method="post">
+          <div className="form-group">
+            <input
+              type="text"
+              name="name"
+              placeholder="Enter your name."
+              className="form-control"
+              required
+            />
+          </div>
+          <div className="form-group">
+            <input
+              type="email"
+              name="email"
+              placeholder="Enter your email"
+              className="form-control"
+              required
+            />
+          </div>
+          <div className="form-group">
+            <input
+              type="password"
+              name="password"
+              placeholder="Enter your password"
+              className="form-control"
+            />
+          </div>
+          <div className="form-group">
+            <input
+              type="password"
+              name="confirmPassword"
+              placeholder="Confirm password"
+              className="form-control"
+            />
+          </div>
+          <div className="form-group">
+            <button
+              type="submit"
+              className="btn btn-block"
+              disabled={navigation.state === "submitting"}
+            >
+              {navigation.state === "submitting"
+                ? "Registering..."
+                : "Register"}
+            </button>
+          </div>
+        </Form>
+      </section>
+    </>
+  )
+}
+
+export default Register
