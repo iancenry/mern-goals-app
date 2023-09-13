b90db9fec130db8eaf370494486aba4bff3f1571
Ian Cenry <iancenry@gmail.com>
2023-09-13 22:58:31 +0300

    feat: define routes

---
+++ frontend/src/pages/index.ts
@@ -0,0 +1,5 @@
+import Dashboard from "./Dashboard"
+import Login, { action } from "./Login"
+import Register, { registerAction } from "./Register"
+
+export { Dashboard, Login, Register, action, registerAction }
