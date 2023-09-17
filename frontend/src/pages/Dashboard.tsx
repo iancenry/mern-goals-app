2f095a473ff414d06475ae592cffec2655667995
Ian Cenry <iancenry@gmail.com>
2023-09-17 13:54:49 +0300

    chore: protect dashboard route

--- frontend/src/pages/Dashboard.tsx
+++ frontend/src/pages/Dashboard.tsx
@@ -1,5 +1,17 @@
-const Dashboard = () => {
-  return <div>Dashboard</div>;
-};
+import { useEffect } from "react"
+import { useNavigate } from "react-router-dom"
+import { useAppSelector } from "../app/hooks"
 
-export default Dashboard;
+const Dashboard = () => {
+  const navigate = useNavigate()
+
+  const { user } = useAppSelector((state) => state.auth)
+
+  useEffect(() => {
+    if (!user) navigate("/login")
+  }, [user, navigate])
+
+  return <div>Dashboard</div>
+}
+
+export default Dashboard
