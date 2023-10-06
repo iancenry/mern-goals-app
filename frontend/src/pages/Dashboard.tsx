239a0ebf50a2d3850608b56923a6ece0141530b2
Ian Cenry <iancenry@gmail.com>
2023-10-06 23:17:43 +0300

    chore: complete dashboard UI

--- frontend/src/pages/Dashboard.tsx
+++ frontend/src/pages/Dashboard.tsx
@@ -1,17 +1,39 @@
 import { useEffect } from "react"
 import { useNavigate } from "react-router-dom"
-import { useAppSelector } from "../app/hooks"
+import { useAppSelector, useAppDispatch } from "../app/hooks"
 import GoalForm from "../components/GoalForm"
+
+import Spinner from "../components/Spinner"
+import { getGoals, reset } from "../features/goals/goalSlice"
+import GoalItem from "../components/GoalItem"
 
 const Dashboard = () => {
   const navigate = useNavigate()
+  const dispatch = useAppDispatch()
 
   const { user } = useAppSelector((state) => state.auth)
 
+  const { goals, isLoading, isError, message } = useAppSelector(
+    (state) => state.goals
+  )
+
   useEffect(() => {
+    if (isError) {
+      console.log(message)
+    }
+
     // if not logged in redirect to login
     if (!user) navigate("/login")
-  }, [user, navigate])
+
+    dispatch(getGoals())
+    // TODO : Fix
+    //unmount goals on logout
+    // return () => dispatch(reset())
+  }, [user, navigate, isError, message, dispatch])
+
+  if (isLoading) {
+    return <Spinner />
+  }
 
   return (
     <>
@@ -21,6 +43,18 @@ const Dashboard = () => {
       </section>
 
       <GoalForm />
+
+      <div className="content">
+        {goals.length > 0 ? (
+          <div className="goals">
+            {goals.map((goal) => (
+              <GoalItem key={goal._id} goal={goal} />
+            ))}
+          </div>
+        ) : (
+          <h3>You have not set any goals</h3>
+        )}
+      </div>
     </>
   )
 }
